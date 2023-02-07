3f4f7e596e90316612ef5223c85422c2a6153014
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-07 12:21:03 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,30 @@
+export const item = {
+    "id": "fbb60619-6e99-4573-8c71-1d5e4f43e3bf",
+    "scan_qr_title": "Able through computer meet author southern record college. Ok international majority need study. Moment ago girl matter site shoulder edge. Region business drop win create move until.",
+    "about_qr": "Sort relationship culture continue level some. Without risk style.",
+    "tick_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png",
+    "connection_status": "Star state force including decade drop. Man peace alone attorney specific nothing else particularly.",
+    "peer_suggestion": "Suffer alone start decision see number. Argue science particularly must. Marriage pull magazine produce attack him.",
+    "all_steps": [
+        {
+            "id": "7ba7aaf9-48bd-4137-b371-708466db55cf",
+            "step": "Face catch test. Down on reach weight near late yes half. Down work up step dog relationship when. During sign century law."
+        },
+        {
+            "id": "a821acb9-b172-4dad-917a-9ab25234f147",
+            "step": "Million cup design seat. System treat president film large."
+        },
+        {
+            "id": "f83dff99-e7f4-4430-a4e2-f3bb22256c61",
+            "step": "Method very news maybe century future. Again actually decide recently alone walk down. Large level reach population."
+        },
+        {
+            "id": "0e1a0989-24e7-4a80-80dc-af2a0804d283",
+            "step": "Increase rest almost economic. Consumer practice dark but attorney rise successful. Traditional simple box lot help any response hear."
+        },
+        {
+            "id": "61793815-13af-463c-b3bc-83671734d45f",
+            "step": "Together answer particularly perhaps who. Nothing southern offer upon everything fight pick. Political guess require political difficult true role card. Society argue recognize take their ability probably."
+        }
+    ]
+}
